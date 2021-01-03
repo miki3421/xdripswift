402e9f933d0cef9b891d693c36a2f192d4442f54
Johan Degraeve <johan.degraeve@gmail.com>
2021-01-03 09:05:18 +0100

    test

--- xdrip/Managers/Loop/LoopManager.swift
+++ xdrip/Managers/Loop/LoopManager.swift
@@ -86,6 +86,73 @@ public class LoopManager:NSObject {
         
         UserDefaults.standard.readingsStoredInSharedUserDefaultsAsDictionary = dictionary
         
+        let decoded = try? JSONSerialization.jsonObject(with: data, options: [])
+        guard let sgvs = decoded as? Array<AnyObject> else {
+            debuglogging("Failed to decode SGVs as array from recieved data.")
+            return
+        }
+        
+        var transformed: Array<Glucose> = []
+        for sgv in sgvs {
+            // Collector might not be available
+            var collector : String? = nil
+            if let _col = sgv["Collector"] as? String {
+                collector = _col
+            }
+            
+            if let glucose = sgv["Value"] as? Int, let trend = sgv["Trend"] as? Int, let dt = sgv["DT"] as? String {
+                transformed.append(Glucose(
+                    glucose: UInt16(glucose),
+                    trend: UInt8(trend),
+                    timestamp: self.parseDate(dt),
+                    collector: collector
+                ))
+            } else {
+                debuglogging("Failed to decode an SGV record.")
+            }
+        }
+        
+        for element in transformed {
+            debuglogging("element timestamp " + element.timestamp.toString(timeStyle: .medium, dateStyle: .none) + ",element value " + element.glucose.description)
+            
+        }
+        
+        
     }
     
+    private func parseDate(_ wt: String) -> Date {
+        // wt looks like "/Date(1462404576000)/"
+        do {
+            let re = try NSRegularExpression(pattern: "\\((.*)\\)")
+            if let match = re.firstMatch(in: wt, range: NSMakeRange(0, wt.count)) {
+                #if swift(>=4)
+                let matchRange = match.range(at: 1)
+                #else
+                let matchRange = match.rangeAt(1)
+                #endif
+                let epoch = Double((wt as NSString).substring(with: matchRange))! / 1000
+                return Date(timeIntervalSince1970: epoch)
+            } else {
+                debuglogging("parsedate error")
+            }
+
+        } catch _ {
+            
+            debuglogging("error")
+            return Date()
+            
+        }
+        
+        return Date()
+    }
+    
+}
+
+
+
+ struct Glucose {
+    public let glucose: UInt16
+    public let trend: UInt8
+    public let timestamp: Date
+    public let collector: String?
 }
